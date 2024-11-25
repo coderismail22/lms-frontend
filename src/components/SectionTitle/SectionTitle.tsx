ef982e84f0d0e871c2c3bb0fccc4dfd02b6b7a82
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-11-25 21:46:04 +0600

    update section title and add testimonial section to the home page

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,17 +1,31 @@
 const SectionTitle = ({
   title,
   subtitle,
+  titleStyles,
+  subTitleStyles,
 }: {
   title: string;
   subtitle?: string;
+  titleStyles?: string;
+  subTitleStyles?: string;
 }) => {
   return (
     <div>
       <div className="mb-4">
-        <h1 className="text-5xl font-bold mb-4 text-center text-slate-400">
+        <h1
+          className={`text-5xl font-bold mb-4 text-center ${
+            titleStyles ? titleStyles : `text-slate-400`
+          }`}
+        >
           {title}{" "}
         </h1>
-        <p className="text-center text-xl mt-2 text-white">{subtitle}</p>
+        <p
+          className={` text-center text-xl mt-2 ${
+            subTitleStyles ? subTitleStyles : `text-white`
+          }`}
+        >
+          {subtitle}
+        </p>
       </div>
     </div>
   );
--- src/pages/Home/Home/Home.tsx
+++ src/pages/Home/Home/Home.tsx
@@ -2,6 +2,7 @@ import Category from "@/components/Category/Category";
 import Hero from "@/components/Hero/Hero";
 import Numbers from "@/components/Numbers/Numbers";
 import PopularCourses from "@/components/PopularCourses/PopularCourses";
+import Testimonial from "@/components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Category />
       <Numbers />
       <PopularCourses />
+      <Testimonial />
     </div>
   );
 };
