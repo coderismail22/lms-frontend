3d8974c61125c30619f201054527cf9a5a763ec7
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-11-25 10:09:54 +0600

    persist the auth state

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,22 @@ import "./index.css";
 import { router } from "./routes/router.tsx";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 const queryClient = new QueryClient();
+
+// Use localStorage to persist the query cache
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+// Persist the query cache on page reloads
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 Days
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
