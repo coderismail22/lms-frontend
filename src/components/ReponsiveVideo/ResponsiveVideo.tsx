5eaaaa359b195475ee662115dd4fdde7bb6f4007
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-12-01 20:46:57 +0600

    fix url issue with yt video

--- src/components/ReponsiveVideo/ResponsiveVideo.tsx
+++ src/components/ReponsiveVideo/ResponsiveVideo.tsx
@@ -5,11 +5,21 @@ interface ResponsiveVideoProps {
 }
 
 const ResponsiveVideo: React.FC<ResponsiveVideoProps> = ({ url }) => {
+  // Check if the URL is a YouTube "watch" URL and convert it to an embed URL
+  const embedUrl = url.includes("youtube.com/watch?v=")
+    ? url.replace(
+        "https://www.youtube.com/watch?v=",
+        "https://www.youtube.com/embed/"
+      )
+    : url; // If it's already an embed URL, keep it as is.
+
+  console.log(url);
   return (
     <div className="relative w-full pb-[56.25%] h-0 overflow-hidden rounded-md shadow-md">
       <iframe
         className="absolute top-0 left-0 w-full h-full"
-        src={url}
+        // src={url}
+        src={embedUrl}
         title="Video Content"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
