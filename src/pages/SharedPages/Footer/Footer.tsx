ad1b85728af8fd13da3fa4bc8aff6ed42374e7eb
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-12-02 16:46:29 +0600

    update coursedetailspageforall

--- src/pages/SharedPages/Footer/Footer.tsx
+++ src/pages/SharedPages/Footer/Footer.tsx
@@ -10,7 +10,11 @@ const Footer = () => {
     <div>
       <footer className="footer bg-[#0F172A] text-gray-300  p-2 text-[16px] flex flex-col md:flex-row items-center justify-center gap-2 md:gap-10">
         <div className="flex flex-col items-center justify-center">
-          <img src="/ic.gif" alt="Ismail Codes" className="w-[100px]" />
+          <img
+            src="/ejobsit-logo.svg"
+            alt="Ismail Codes"
+            className="w-[100px]"
+          />
         </div>
         <div className="flex flex-col items-center">
           <p className="text-right uppercase text-sm font-bold">Follow us</p>
@@ -36,7 +40,7 @@ const Footer = () => {
       <footer className="footer footer-center bg-[#26283b] text-white rounded px-10 py-1">
         <div>
           <p className="text-[12px] uppercase text-center font-semibold">
-            Copyright © {new Date().getFullYear()} Expert Webian LMS
+            Copyright © {new Date().getFullYear()} Al Falah Web Solutions
           </p>
         </div>
       </footer>
