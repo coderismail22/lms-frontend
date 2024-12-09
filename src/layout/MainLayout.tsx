cb642280b4d9ea62c2a6391bc23da2cc4ad2115c
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2024-12-10 00:49:13 +0600

    add scroll to top

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../pages/SharedPages/Navbar/Navbar";
 import Footer from "../pages/SharedPages/Footer/Footer";
 import { TopBanner } from "../components/TopBanner/TopBanner";
+import ScrollToTop from "@/components/ScrollToTop/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900">
       <TopBanner />
       <Navbar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </div>
