dd23480613a128ea7f78bd5c5310d3a911ad9f99
Md Ismail Hossain <122710226+coderismail22@users.noreply.github.com>
2025-01-13 20:47:04 +0600

    update the contact page animation threshold

--- src/schemas/register.schema.ts
+++ src/schemas/register.schema.ts
@@ -3,7 +3,11 @@ import { z } from "zod";
 export const registerSchema = z
   .object({
     name: z.string().min(1, "Full Name is required"),
-    email: z.string().email("Invalid email format").min(1, "Email is required"),
+    email: z
+      .string()
+      .email("Invalid email format")
+      .min(1, "Email is required")
+      .transform((email) => email.toLowerCase()),
     password: z.string().min(6, "Password must be at least 6 characters long"),
     confirmPassword: z.string().min(1, "Confirm Password is required"),
   })
